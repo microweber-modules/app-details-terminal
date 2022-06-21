265f58175852b2914619408e042d69dd4dbe691a
Bozhidar Slaveykov <bobi@microweber.com>
2022-06-21 17:30:58 +0300

    update

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$config = array();
+$config['name'] = "App details terminal";
+$config['author'] = "Bozhidar Slaveykov";
+$config['ui'] = false; //if set to true, module will be visible in the toolbar
+$config['ui_admin'] = false; //if set to true, module will be visible in the admin panel
+$config['categories'] = "content";
+$config['position'] = 99;
+$config['version'] = 0.1;
---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+
+// If module not loading just call artisan command "microweber:reload_database"
+
+\Artisan::command('microweber:get-templates', function () {
+
+
+
+});
+
+\Artisan::command('microweber:get-modules', function () {
+
+
+});
+
+\Artisan::command('microweber:get-languages', function () {
+
+
+});
