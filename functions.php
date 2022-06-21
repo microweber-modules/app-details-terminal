7bda2bba269b8aa1d91e451de59cf3e27356840d
Bozhidar Slaveykov <bobi@microweber.com>
2022-06-21 17:40:14 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,8 +4,20 @@
 
 \Artisan::command('microweber:get-templates', function () {
 
+    $templates = [];
+    $getTemplates = site_templates();
+    if (!empty($getTemplates)) {
+        foreach ($getTemplates as $template) {
+            $templates[] = [
+                'name'=>$template['name'],
+                'author'=>$template['author'],
+                'version'=>$template['version'],
+                'dir_name'=>$template['dir_name'],
+            ];
+        }
+    }
 
-
+    echo json_encode($templates);
 });
 
 \Artisan::command('microweber:get-modules', function () {
