69d524a4de1a3885ccc72fda972106fcc254c40f
Bozhidar Slaveykov <bobi@microweber.com>
2022-06-28 17:43:42 +0300

    Update config.php

--- config.php
+++ config.php
@@ -6,4 +6,4 @@ $config['ui'] = false; //if set to true, module will be visible in the toolbar
 $config['ui_admin'] = false; //if set to true, module will be visible in the admin panel
 $config['categories'] = "content";
 $config['position'] = 99;
-$config['version'] = 0.1;
+$config['version'] = 0.2;
